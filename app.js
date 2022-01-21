9e8619c9941a7d65f7c2c49bbe04477b0873b4a2
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-01-21 09:21:30 -0300

    ajustando back

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 //criar Schema.
@@ -14,44 +12,26 @@ const itemSchema = {
 
 const Item = mongoose.model("Item", itemSchema);
 
-//criar uma instância.
-const item1 = new Item({ name: "Arroz" });
-const item2 = new Item({ name: "Feijão" });
-const item3 = new Item({ name: "Macarrão" });
-
-const d = [item1, item2, item3];
-
 //enviar solicitação para servidor.
-app.get("/", function (req, res) {
-  Item.find({}, function (err, f) {
-    if (f.length === 0) {
-      Item.insertMany(d, function (err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Successfully saved items to database");
-        }
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", { newListItem: f });
-    }
-  });
+app.get("/", async (req, res) => {
+  const f = await Item.find({})
+  res.json({ newListItem: f });
 });
 
 // para enviar modificação.
-app.post("/", function (req, res) {
-  i = req.body.n;
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const i = req.body.n;
   const item = new Item({
     name: i,
   });
   item.save();
-  res.redirect("/");
+  res.json({ item })
 })
 
 //deletar item.
-app.post("/delete", function (req, res) {
-  Item.findByIdAndRemove(req.body.checkbox, function (err) {
+app.delete("/delete", (req, res) => {
+  Item.findByIdAndRemove(req.body.checkbox, (err) => {
     if (!err) {
       console.log("Successfully deleted");
       res.redirect("/");
@@ -60,6 +40,6 @@ app.post("/delete", function (req, res) {
 });
 
 //abrir projeto dentro de qualquer porta.
-app.listen(3001, function () {
+app.listen(3001, () => {
   console.log("listening on port 3001.");
 });
