29b35be1cab23ffba0ed4cd3235a4395b3451c65
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-01-28 12:54:53 -0300

    reajuste

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-const { TestWatcher } = require("jest");
-var app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
 app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-//criar Schema.
+//abrir projeto dentro de qualquer porta.
+app.listen(3002, () => {
+  console.log("listening on port 3002.");
+});
+
+// Criando Schema.
 const itemSchema = {
-  name: String,
+  name: String
 };
 
+// Iniciamos a model.
 const Item = mongoose.model("Item", itemSchema);
 
-//enviar solicitação para servidor.
-app.get("/", async (req, res) => {
-  const f = await Item.find({})
-  res.json({ newListItem: f });
+// Retorna todos os itens.
+app.get("/item", async (req, res) => {
+  const item = await Item.find({});
+  res.send(item);
 });
 
-// para enviar modificação.
-app.post("/", (req, res) => {
-  console.log(req.body);
-  const i = req.body.n;
+// Salvar um item.
+app.post("/item", async (req, res) => {
+  const idData = req.body;
   const item = new Item({
-    name: i,
+    name: idData.name
   });
-  item.save();
-  res.json({ item })
+  const currentItem = await item.save();
+  res.send(currentItem);
 });
 
-// atualiza item
+// Atualizar item
 app.put("/item/:id", (req,res) => {
   Item.findOneAndUpdate({_id: req.params.id}, {$set: req.body}, (err, doc) => {
     if(err) res.json({sucesso: false})
-    return res.json(doc)
+    return res.json(doc);
   });
 });
 
-//deletar item.
-app.delete("/delete", (req, res) => {
+// Deletar item.
+app.delete("/item/delete", (req, res) => {
   Item.findByIdAndRemove (req.body._id, (err, doc)=> {
     if(err) res.json({sucesso: false})
-    return res.json(doc)
+    return res.json(doc);
   });
 });
 
-//abrir projeto dentro de qualquer porta.
-app.listen(3001, () => {
-  console.log("listening on port 3001.");
-});
