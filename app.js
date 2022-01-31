13d6c3a1e52aac7baacb79f2f196780a44e2cb9b
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-01-31 16:47:05 -0300

    iniciando routes

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const app = express();
 app.use(bodyParser.json());
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-//abrir projeto dentro de qualquer porta.
+// Inicia a aplicação na porta indicada.
 app.listen(3002, () => {
   console.log("listening on port 3002.");
 });
 
 // Criando Schema.
 const itemSchema = {
-  name: String
+  name: String,
+  status: Boolean
 };
 
 // Iniciamos a model.
@@ -20,33 +21,75 @@ const Item = mongoose.model("Item", itemSchema);
 
 // Retorna todos os itens.
 app.get("/item", async (req, res) => {
-  const item = await Item.find({});
-  res.send(item);
+  try {
+    const item = await Item.find({});
+    res.send(item);
+  } catch (error) {
+    res.statuss(400).json({
+      status: "fail",
+      message: error
+    });
+  }
 });
 
-// Salvar um item.
+// Retorna item por id.
+app.get("/item/:id", async (req, res) => {
+  const idItem = req.params.id;
+  try {
+    const item = await Item.findById(idItem)
+    res.send(item);
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error
+    });
+  }
+});
+
+// cria um novo item.
 app.post("/item", async (req, res) => {
   const idData = req.body;
   const item = new Item({
-    name: idData.name
+    name: idData.name,
+    status: idData.status
   });
-  const currentItem = await item.save();
-  res.send(currentItem);
+  try {
+    const currentItem = await item.save();
+    res.send(currentItem);
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error
+    });
+  }
 });
 
-// Atualizar item
-app.put("/item/:id", (req,res) => {
-  Item.findOneAndUpdate({_id: req.params.id}, {$set: req.body}, (err, doc) => {
-    if(err) res.json({sucesso: false})
-    return res.json(doc);
-  });
+// Atualiza item
+app.put("/item/:id", async (req, res) => {
+  const idItem = req.params.id;
+  const item = req.body;
+  try {
+    const itemUpdated = await Item.findByIdAndUpdate(idItem, item);
+    res.send(itemUpdated);
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error
+    });
+  }
 });
 
-// Deletar item.
-app.delete("/item/delete", (req, res) => {
-  Item.findByIdAndRemove (req.body._id, (err, doc)=> {
-    if(err) res.json({sucesso: false})
-    return res.json(doc);
-  });
+// Remove item.
+app.delete("/item/:id", async (req, res) => {
+  const idItem = req.params.id;
+  try {
+    const item = await Item.findByIdAndRemove(idItem);
+    res.send(item);
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error
+    });
+  }
 });
 
